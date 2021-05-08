bb63e44b40d6a0c6389521fed0f52586e2c81a0f
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2021-05-08 19:24:47 +0530

    Data stored in json files

---
+++ src/data/productData1.js
@@ -0,0 +1,17 @@
+export const ProductData1 = [
+    {
+        "image": "https://analyticsindiamag.com/wp-content/uploads/2019/12/How-SMBs-In-India-Can-Unlock-Their-Potential-With-Analytics.jpg",
+        "title": "POTTERY US",
+        "description": "It is one of the oldest and most widespread of the decorative arts, consisting of objects made of clay and hardened with heat. The objects made are commonly useful ones, such as vessels for holding liquids or plates or bowls from which food can be served."
+    },
+    {
+        "image": "https://i.pinimg.com/originals/a6/82/23/a68223a7225e0acbd950e6eb29bc7691.jpg",
+        "title": "SARIKA SARI",
+        "description": "Sarika Fashion deals in full variety of Indian Sarees, Lehenga, Odhani, Western dresses, Wedding dresses, etc."
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1574705477102-1ff256153d63?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fGluZGlhbiUyMG1hcmtldHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "title": "STYLE WAREHOUSE",
+        "description": "This is a one stop place, where you will find every clothing item for daily wear, party, functions or any other rendezvous."
+    }
+]
