b27f5a199e103410e986e12faf92b4e6545ce128
Akshitasaxena22 <akshita.saxena_cs18@gla.ac.in>
2021-05-09 12:04:31 +0530

    Documents

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import Details from './components/pages/Details';
 
